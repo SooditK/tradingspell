08e18adb7d16396857db47259e8026ab52404a5d
SooditK <sooditkumarabc@gmail.com>
2021-11-08 13:18:03 +0530

    🔥 Removed Diwali Theme

--- components/Feature.js
+++ components/Feature.js
@@ -18,41 +18,40 @@ const Feature = () => {
           </div>
           <section className="text-gray-400 bg-gray-900 body-font">
             <div className="container px-5 py-24 mx-auto flex flex-wrap">
-              <div className="flex flex-wrap mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
-                <div className="w-full sm:p-4 px-4 mb-6">
-                  <div className="flex flex-col text-center w-full mt-20">
-                    <div className="flex flex-col text-center w-full mb-16">
-                      {/* <strike className="text-3xl sm:text-5xl text-white font-black mb-5 title-font">
+              <div className="w-full sm:p-4 px-4 mb-6">
+                <div className="flex flex-col text-center w-full mt-20">
+                  <div className="flex flex-col text-center w-full mb-16">
+                    {/* <strike className="text-3xl sm:text-5xl text-white font-black mb-5 title-font">
                 {" "}
                 (₹68485/-){" "}
               </strike> */}
-                      <span className="text-3xl mb-3 text-gray-300">
-                        Program will start from 14th November
+                    <span className="text-3xl mb-3 text-gray-300">
+                      Program will start from 14th November
+                    </span>
+                    <h1 className="text-2xl sm:text-5xl text-white font-medium title-font">
+                      ALL THIS IN JUST{" "}
+                      <span className="my-4 font-bold text-yellow-400 text-6xl flex items-center justify-center ">
+                        ₹10999/-
+                        <p className="mt-8 text-base">Including GST</p>
                       </span>
-                      <h1 className="text-2xl sm:text-5xl text-white font-medium title-font">
-                        ALL THIS IN JUST{" "}
-                        <span className="my-4 font-bold text-yellow-400 text-6xl flex items-center justify-center ">
-                          ₹10999/-
-                          <p className="mt-8 text-base">Including GST</p>
-                        </span>
-                        <strike className="font-light"> (₹19999/-) </strike>
-                        <span className="font-black">45% OFF</span>
-                      </h1>
-                    </div>
-                    <h1 className=" sm:text-3xl text-white text-xl font-medium title-font">
-                      So What are You Waiting for..!! Hop on to the journey to
-                      become financially Free
+                      <strike className="font-light"> (₹19999/-) </strike>
+                      <span className="font-black">45% OFF</span>
                     </h1>
-                    <div className="flex-row sm:flex items-center justify-center mt-10">
-                      <a
-                        href="https://rzp.io/l/MTAP"
-                        className="flex items-center justify-center"
-                      >
-                        <button className="flex rounded-full text-md sm:text-2xl text-black bg-green-300 border-2 py-2 px-4 sm:py-2 sm:px-8 focus:outline-none border-green-300 shadow-2xl hover:border-green-700 hover:bg-green-700 hover:text-white">
-                          Enroll for Master Technical Analysis Program
-                        </button>
-                      </a>
-                      {/* <a
+                  </div>
+                  <h1 className=" sm:text-3xl text-white text-xl font-medium title-font">
+                    So What are You Waiting for..!! Hop on to the journey to
+                    become financially Free
+                  </h1>
+                  <div className="flex-row sm:flex items-center justify-center mt-10">
+                    <a
+                      href="https://rzp.io/l/MTAP"
+                      className="flex items-center justify-center"
+                    >
+                      <button className="flex rounded-full text-md sm:text-2xl text-black bg-green-300 border-2 py-2 px-4 sm:py-2 sm:px-8 focus:outline-none border-green-300 shadow-2xl hover:border-green-700 hover:bg-green-700 hover:text-white">
+                        Enroll for Master Technical Analysis Program
+                      </button>
+                    </a>
+                    {/* <a
                 href="https://rzp.io/l/ZRaXZNJ4L"
                 className="flex items-center justify-center"
                 >
@@ -60,23 +59,13 @@ const Feature = () => {
                 Reserve Your Spot for MTAP (2.0)
                 </button>
               </a> */}
-                    </div>
-                    {/* <p
+                  </div>
+                  {/* <p
                       className={`flex text-lg sm:text-5xl items-center justify-center mt-8 neonText ${styles.neonText}`}
                     >
                       Special Diwali Offer Extra ₹1000 OFF till 7th November
                     </p> */}
-                  </div>
                 </div>
-              </div>
-              <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
-                <Link href="https://rzp.io/l/MTAP">
-                  <img
-                    className="object-cover object-center w-full h-full cursor-pointer"
-                    src="https://i.imgur.com/T8cBuqx.png"
-                    alt="stats"
-                  />
-                </Link>
               </div>
             </div>
           </section>
--- components/mtar/FeatureTable.js
+++ components/mtar/FeatureTable.js
@@ -18,39 +18,38 @@ const FeatureTable = () => {
           </div>
           <section className="text-gray-400 bg-gray-900 body-font">
             <div className="container px-5 py-24 mx-auto flex flex-wrap">
-              <div className="flex flex-wrap mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
-                <div className="w-full sm:p-4 px-4 mb-6">
-                  <div className="flex flex-col text-center w-full mt-20">
-                    <div className="flex flex-col text-center w-full mb-16">
-                      {/* <strike className="text-3xl sm:text-5xl text-white font-black mb-5 title-font">
+              <div className="w-full sm:p-4 px-4 mb-6">
+                <div className="flex flex-col text-center w-full mt-20">
+                  <div className="flex flex-col text-center w-full mb-16">
+                    {/* <strike className="text-3xl sm:text-5xl text-white font-black mb-5 title-font">
                         {" "}
                         (₹68485/-){" "}
                       </strike> */}
-                      <h1 className="text-2xl sm:text-5xl text-white font-medium title-font">
-                        <br />
-                        ALL THIS IN JUST{" "}
-                        <span className="my-4 font-bold text-yellow-400 text-6xl flex items-center justify-center ">
-                          ₹4999/-
-                          <p className="mt-8 text-base">Including GST</p>
-                        </span>
-                        <strike className="font-light"> (₹14999/-) </strike>
-                        <span className="font-black">66.67% OFF</span>
-                      </h1>
-                    </div>
-                    <h1 className=" sm:text-3xl text-yellow-400 text-xl font-medium title-font">
-                      NOTE: THIS TRAINING IS ONLY FOR THOSE WHO HAVE PREVIOUSLY
-                      ATTENDED MTAP
+                    <h1 className="text-2xl sm:text-5xl text-white font-medium title-font">
+                      <br />
+                      ALL THIS IN JUST{" "}
+                      <span className="my-4 font-bold text-yellow-400 text-6xl flex items-center justify-center ">
+                        ₹4999/-
+                        <p className="mt-8 text-base">Including GST</p>
+                      </span>
+                      <strike className="font-light"> (₹14999/-) </strike>
+                      <span className="font-black">66.67% OFF</span>
                     </h1>
-                    <div className="flex-row sm:flex items-center justify-center mt-10">
-                      <a
-                        href="https://rzp.io/l/MTARtradingspell"
-                        className="flex items-center justify-center"
-                      >
-                        <button className="flex rounded-full text-md sm:text-2xl text-black bg-green-300 border-2 py-2 px-4 sm:py-2 sm:px-8 focus:outline-none border-green-300 shadow-2xl hover:border-green-700 hover:bg-green-700 hover:text-white">
-                          Enroll for Master Technical Analysis Revision
-                        </button>
-                      </a>
-                      {/* <a
+                  </div>
+                  <h1 className=" sm:text-3xl text-yellow-400 text-xl font-medium title-font">
+                    NOTE: THIS TRAINING IS ONLY FOR THOSE WHO HAVE PREVIOUSLY
+                    ATTENDED MTAP
+                  </h1>
+                  <div className="flex-row sm:flex items-center justify-center mt-10">
+                    <a
+                      href="https://rzp.io/l/MTARtradingspell"
+                      className="flex items-center justify-center"
+                    >
+                      <button className="flex rounded-full text-md sm:text-2xl text-black bg-green-300 border-2 py-2 px-4 sm:py-2 sm:px-8 focus:outline-none border-green-300 shadow-2xl hover:border-green-700 hover:bg-green-700 hover:text-white">
+                        Enroll for Master Technical Analysis Revision
+                      </button>
+                    </a>
+                    {/* <a
                 href="https://rzp.io/l/ZRaXZNJ4L"
                 className="flex items-center justify-center"
                 >
@@ -58,23 +57,13 @@ const FeatureTable = () => {
                 Reserve Your Spot for MTAP (2.0)
                 </button>
               </a> */}
-                    </div>
-                    {/* <p
+                  </div>
+                  {/* <p
                       className={`flex text-lg sm:text-5xl items-center justify-center mt-8 neonText ${styles.neonText}`}
                     >
                       Special Diwali Offer Extra ₹1000 OFF till 7th November
                     </p> */}
-                  </div>
                 </div>
-              </div>
-              <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
-                <Link href="https://rzp.io/l/MTAP">
-                  <img
-                    className="object-cover object-center w-full h-full cursor-pointer"
-                    src="https://i.imgur.com/bBNca9V.png"
-                    alt="stats"
-                  />
-                </Link>
               </div>
             </div>
           </section>
